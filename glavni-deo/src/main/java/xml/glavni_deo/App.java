3802d154f82e9499165f56fbbc121b031c98be6d
milicaz <zm025555@gmail.com>
2020-05-21 13:18:21 +0200

    Ubacen model za glavni deo Ubacen poseban folder sa semama

---
+++ glavni-deo/src/main/java/xml/glavni_deo/App.java
@@ -0,0 +1,13 @@
+package xml.glavni_deo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
