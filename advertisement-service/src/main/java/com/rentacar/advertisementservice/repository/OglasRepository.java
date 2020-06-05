586d39aaaf8166fb3081ab1986c8f6a618420cae
milicaz <zm025555@gmail.com>
2020-06-05 20:18:17 +0200

    Izmene Oglas

--- advertisement-service/src/main/java/com/rentacar/advertisementservice/repository/OglasRepository.java
+++ advertisement-service/src/main/java/com/rentacar/advertisementservice/repository/OglasRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rentacar.advertisementservice.model.Oglas;
 
+@Repository
 public interface OglasRepository extends JpaRepository<Oglas, Long> {
 
 	List<Oglas> findAll();
